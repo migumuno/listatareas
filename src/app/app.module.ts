9b4ce0c3b08b0acb3264b2593142c5bf25239c4c
DelismaraCamara <delismaracamara@gmail.com>
2020-09-24 11:04:01 +0200

    Se agregó el contenedor de diálogo para mostrar contenido en una ventana superpuesta.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListadetareaComponent } from './listadetarea/listadetarea.component';
 import {DialogModule} from 'primeng/dialog';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
 	declarations: [AppComponent, ListadetareaComponent],
 	imports: [
 		BrowserModule,
 		AppRoutingModule,
-		DialogModule
+		DialogModule,
+		BrowserAnimationsModule,
+		ButtonModule
 	],
 	providers: [],
 	bootstrap: [AppComponent],
--- src/app/listadetarea/listadetarea.component.ts
+++ src/app/listadetarea/listadetarea.component.ts
@@ -1,15 +1,50 @@
 import { Component, OnInit } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-listadetarea',
   templateUrl: './listadetarea.component.html',
-  styleUrls: ['./listadetarea.component.scss']
-})
-export class ListadetareaComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  styles: [`
+  :host ::ng-deep .p-button {
+      margin: 0 .5rem 0 0;
+      min-width: 10rem;
+      background-color: cornflowerblue;
   }
 
+  p {
+      margin: 0;
+  }
+
+  .confirmation-content {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+  }
+
+  :host ::ng-deep .p-dialog .p-button {
+      min-width: 6rem;
+      margin-top: 1rem;
+  }
+`]
+})
+
+export class ListadetareaComponent implements OnInit {
+
+  constructor(private primengConfig: PrimeNGConfig) {}
+
+  ngOnInit(): void {
+    this.primengConfig.ripple = true;
+  }
+
+  displayBasic2: boolean;
+  displayPosition: boolean;
+  position: string;
+
+  showBasicDialog2() {
+    this.displayBasic2 = true;
+}
+    showPositionDialog(position: string) {
+      this.position = position;
+      this.displayPosition = true;
+  }
 }
